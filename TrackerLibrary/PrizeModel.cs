114f2f0b1a57d5860d8562271da6e640ea55291f
jon-maddocks <56932287+jon-maddocks@users.noreply.github.com>
2022-12-29 15:29:30 -0500

    Lesson 9 completed. Setup for SQL for CreatePrize is started.

--- TrackerLibrary/PrizeModel.cs
+++ TrackerLibrary/PrizeModel.cs
@@ -8,9 +8,32 @@ namespace TrackerLibrary
 {
     public class PrizeModel
     {
+        public int id { get; set; }
         public int PLaceNumber { get; set; }
         public string PlaceName { get; set; }
         public decimal PrizeAmount { get; set; }
         public double PrizePercentage { get; set; }
+
+        public PrizeModel()
+        { 
+            
+        }
+
+        public PrizeModel(string pLaceNumber, string placeName, string prizeAmount, string prizePercentage)
+        {
+            PlaceName = pLaceNumber;
+
+            int placeNumberValue = 0;
+            int.TryParse(pLaceNumber, out placeNumberValue);
+            PLaceNumber = placeNumberValue;
+
+            decimal prizeAmountValue = 0;
+            decimal.TryParse(prizeAmount, out prizeAmountValue);
+            PrizeAmount = prizeAmountValue;
+
+            double prizePercentageValue = 0;
+            double.TryParse(prizePercentage, out prizePercentageValue);
+            PrizePercentage = prizePercentageValue;
+        }
     }
 }
--- TrackerUI/CreatePrize.cs
+++ TrackerUI/CreatePrize.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TrackerLibrary;
 
 namespace TrackerUI
 {
@@ -15,6 +16,71 @@ namespace TrackerUI
         public CreatePrize()
         {
             InitializeComponent();
+        }
+
+        private void btnCreatePrize_Click(object sender, EventArgs e)
+        {
+            if (ValidateForm())
+            {
+                PrizeModel model = new PrizeModel(
+                    txtPlaceName.Text,
+                    txtPlaceNumber.Text,
+                    txtPrizeAmount.Text,
+                    txtPrizePercentage.Text);
+
+                foreach (IDataConnection db in GlobalConfig.Connections)
+                {
+                    db.CreatePrize(model);
+                }
+                    txtPlaceName.Text = "";
+                    txtPlaceNumber.Text = "";
+                    txtPrizeAmount.Text = "0";
+                    txtPrizePercentage.Text = "0";
+            } else
+            {
+                MessageBox.Show("This form has invalid information");
+            }
+        }
+
+        private bool ValidateForm()
+        {
+            bool output = true;
+            int placeNumber = 0;
+            bool placeNumberValidNumber = int.TryParse(txtPlaceNumber.Text, out placeNumber);
+
+            if (!placeNumberValidNumber) 
+            {
+                output = false;
+            }
+            if(placeNumber < 1)
+            {
+                output = false;
+            }
+            if (txtPlaceName.Text.Length == 0)
+            {
+                output = false;
+            }
+
+            decimal prizeAmount = 0;
+            double prizePercentage = 0;
+
+            bool prizeAmountValid = decimal.TryParse(txtPrizeAmount.Text, out prizeAmount);
+            bool prizePercentageValid = double.TryParse(txtPrizePercentage.Text, out prizePercentage);
+
+            if(!prizeAmountValid || !prizePercentageValid)
+            {
+                output= false;
+            }
+            if(prizeAmount <=0 && prizePercentage <=0)
+            {
+                output = false;
+            }
+            if(prizePercentage < 0 || prizePercentage > 100)
+            {
+                output = false;
+            }
+
+            return output;
         }
     }
 }
